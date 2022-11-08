a9ea5de257298a95d20caf0b3be6ec3dd85f9509
Hiddenache <hiddenache01@gmail.com>
2022-11-08 20:22:29 +0200

    Add project files.

---
+++ Firma_Distributie/Actiuni.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Firma_Distributie
+{
+    public partial class Actiuni : Form
+    {
+        public Actiuni()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 f = new Form1();
+            f.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Firma_Distributie/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Firma_Distributie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Furnizor furnizor = new Furnizor();
+            furnizor.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Articol articol = new Articol();
+            articol.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PunctDesfacere punctDesfacere = new PunctDesfacere();
+            punctDesfacere.Show();
+            this.Hide();
+        }
+    }
+}
